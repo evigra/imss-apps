6d033d339414d7899783c6d1f46d1f823094c9be
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-02-18 21:56:19 -0600

    Update

--- modulos/modulo/index.php
+++ modulos/modulo/index.php
@@ -1,6 +1,8 @@
 <?php	
 	$objeto											=new modulo();		
 	$objeto->__SESSION();
+	$_SESSION["pdf"]["subject"]						="DIRECCION DE PRESTACIONES ECONOMICAS Y SOCIALES";
+	
 	#$objeto->__PRINT_R($_SESSION);
 	
 	# CARGANDO PLANTILLAS GENERALES
@@ -71,6 +73,9 @@
 
     		    							
 		
+		#$objeto->__GENERAR_PDF();
+
+		
     	$module_title								="Modificar ";
     }	
     elseif($objeto->sys_section=="show")
@@ -109,7 +114,6 @@
 	   	$data										=$objeto->__BROWSE();
     	$objeto->words["module_body"]               =$objeto->__VIEW_KANBAN($template_body,$data["data"]);	
     }    
-	/*
     elseif($objeto->sys_section=="report_especifico")
     {
 		#BOTONES SECCION DERECHA
@@ -155,7 +159,7 @@
 		$objeto->words["module_body"]				=$data["html"];
 		$module_title								="Reporte de Pendientes de ";
     }
-    elseif($objeto->sys_section=="report_aprovados")
+    elseif($objeto->sys_section=="report_si")
     {
 		#BOTONES SECCION DERECHA
 		$module_right=array(
@@ -166,11 +170,11 @@
 		);
 
 		#CARGANDO VISTA PARTICULAR Y CAMPOS			
-		$data										= $objeto->__REPORT_APROVADO();
+		$data										= $objeto->__REPORT_SI();
 		$objeto->words["module_body"]				=$data["html"];
-		$module_title								="Reporte Aprovados de ";
+		$module_title								="Calificadas SI en ";
     }
-    elseif($objeto->sys_section=="report_cancelados")
+    elseif($objeto->sys_section=="report_no")
     {
 		#BOTONES SECCION DERECHA
 		$module_right=array(
@@ -180,70 +184,196 @@
 		    array("report"=>"Reporte"),
 		);
 
+		
 		#CARGANDO VISTA PARTICULAR Y CAMPOS			
-		$data										= $objeto->__REPORT_CANCELADOS();
+		$data										= $objeto->__REPORT_NO();
 		$objeto->words["module_body"]				=$data["html"];
-		$module_title								="Reporte de Cancelados de ";
+
+		$objeto->words["asunto_titular"]			="<b>C. MARTIN LUPERCIO CHAVEZ</b>";
+		$objeto->words["asunto_puesto"]				="ENC,. DEL DEPTO DE PRESTACIONES ECONOMICAS";		
+		$objeto->words["asunto_descripcion"]		="ENTREGO DEVUELTA INCAPACIDAD ORIGINALCON COPIA ST7  ATENDIDO Y CALIFICADO COMO DE NO TRABAJO/TRAYECTO/RECAIDA";
+		
+		
+		$module_title								="Calificadas NO en ";
+    }
+	
+    elseif($objeto->sys_section=="report_sol")
+    {
+		#CARGANDO VISTA PARTICULAR Y CAMPOS			
+		$data										= $objeto->__REPORT_SOL();		
+		$objeto->words["module_body"]				=$data["html"];
+		
+		$objeto->words["asunto_titular"]			="<b>C. MARTIN LUPERCIO CHAVEZ</b>";
+		$objeto->words["asunto_puesto"]				="ENC,. DEL DEPTO DE PRESTACIONES ECONOMICAS";		
+		$objeto->words["asunto_descripcion"]		="SOLICITO A USTED INCAPACIDADES DEL PACIENTE QUE DETALLO A CONTINUACION PARA TRAMITE EN ESTE DEPARTAMENTO.";
+
+		$module_title								="Solicitud de ";
     }    
-	*/
+
+    elseif($objeto->sys_section=="report_fs")
+    {
+		#CARGANDO VISTA PARTICULAR Y CAMPOS			
+		$data										= $objeto->__REPORT_FS();		
+		$objeto->words["module_body"]				=$data["html"];
+		
+		$objeto->words["asunto_titular"]			="<b>C.LUIS FERNANDO MUÑOZ BERNAL</b>";
+		$objeto->words["asunto_puesto"]				="ENC DEPTO. DE AFILIACION Y VIGENCIA";		
+		$objeto->words["asunto_descripcion"]		="POR MEDIO DEL PRESENTE  SOLICITO A USTED F1 Y F3 DE LOS PACIENTES QUE A CONTINUACION DETALLO.";
+
+		$module_title								="F1 y F3 de ";
+    }    
+
+    elseif($objeto->sys_section=="report_dev")
+    {
+		#CARGANDO VISTA PARTICULAR Y CAMPOS			
+		$data										= $objeto->__REPORT_DEV();		
+		$objeto->words["module_body"]				=$data["html"];
+		
+		$objeto->words["asunto_titular"]			="<b>C. MARTIN LUPERCIO CHAVEZ</b>";
+		$objeto->words["asunto_puesto"]				="ENC,. DEL DEPTO DE PRESTACIONES ECONOMICAS";		
+		$objeto->words["asunto_descripcion"]		="DEVUELVO A USTED INCAPACIDADES DE PACIENTES QUE NO SE HAN PRESENTADO PARA REALIZAR SU CALIFICACION, MISMAS QUE DETALLO A CONTINUACION. ";
+
+		$module_title								="Devolucion de ";
+    }    
+	
+    elseif($objeto->sys_section=="report_carta_realizada")
+    {
+		$module_center=array(		    
+			array("cartas_patrones"=>"CARTAS"),	
+			array("sobres_patrones"=>"SOBRES"),						
+		);		
+		#CARGANDO VISTA PARTICULAR Y CAMPOS			
+		$data										= $objeto->__REPORT_CAR_REALIZADA();		
+		$objeto->words["module_body"]				=$data["html"];
+		
+		$objeto->words["asunto_titular"]			="<b>C. MARTIN LUPERCIO CHAVEZ</b>";
+		$objeto->words["asunto_puesto"]				="ENC,. DEL DEPTO DE PRESTACIONES ECONOMICAS";		
+		$objeto->words["asunto_descripcion"]		="DEVUELVO A USTED INCAPACIDADES DE PACIENTES QUE NO SE HAN PRESENTADO PARA REALIZAR SU CALIFICACION, MISMAS QUE DETALLO A CONTINUACION. ";
+
+		$module_title								="Devolucion de ";
+    }    
+    elseif($objeto->sys_section=="carta_patron")
+    {
+		#CARGANDO VISTA PARTICULAR Y CAMPOS			
+		
+		#$objeto->words["module_body"]				=$data["html"];
+		
+		$objeto->words["asunto_titular"]			="<b>C. MARTIN LUPERCIO CHAVEZ</b>";
+		$objeto->words["asunto_puesto"]				="ENC,. DEL DEPTO DE PRESTACIONES ECONOMICAS";		
+		$objeto->words["asunto_descripcion"]		="DEVUELVO A USTED INCAPACIDADES DE PACIENTES QUE NO SE HAN PRESENTADO PARA REALIZAR SU CALIFICACION, MISMAS QUE DETALLO A CONTINUACION. ";
+
+		$module_title								="Devolucion de ";
+		$_SESSION["pdf"]["template"]			=array(
+			array(
+				"format"		=>array(104,241),					
+				"html"			=>$objeto->__FORMATO_SOBRE($objeto->sys_primary_id),					
+				"orientation"	=>"L",					
+			),						
+			array(
+				"format"		=>"A4",					
+				"html"			=>$objeto->__FORMATO_CARTA($objeto->sys_primary_id),					
+				"orientation"	=>"P",					
+			),						
+		);		
+    }    
+    elseif($objeto->sys_section=="sobres_patrones")
+    {
+		#CARGANDO VISTA PARTICULAR Y CAMPOS			
+
+		$datas									= $objeto->__REPORT_CAR_REALIZADA();		
+		$_SESSION["pdf"]["template"]			=array();
+		
+		
+		foreach($datas["data"] as $data)
+		{
+			$_SESSION["pdf"]["template"][]=	array(
+				"format"		=>array(104,241),					
+				"html"			=>$objeto->__FORMATO_SOBRE($data["id"]),					
+				"orientation"	=>"L",								
+			);			
+		}
+		
+		
+		#$objeto->__PRINT_R($data);
+
+		/*
+
+		$module_title								="Devolucion de ";
+		$_SESSION["pdf"]["template"]			=array(
+			array(
+				"format"		=>array(104,241),					
+				"html"			=>$objeto->__FORMATO_SOBRE($objeto->sys_primary_id),					
+				"orientation"	=>"L",					
+			),						
+			array(
+				"format"		=>"A4",					
+				"html"			=>$objeto->__FORMATO_CARTA($objeto->sys_primary_id),					
+				"orientation"	=>"P",					
+			),						
+		);
+*/		
+    }    
+
     else
     {
 		#BOTONES SECCION DERECHA
-		$module_right=array(
-		    array("create"=>"Crear"),
-		    #array("write"=>"Modificar"),
-		    array("kanban"=>"Kanban"),
-		    array("report"=>"Reporte"),		    
-		);
-
 		#CARGANDO VISTA PARTICULAR Y CAMPOS
-		$option["template_title"]	                = $objeto->sys_module . "html/report_estatus_title";
-		$option["template_body"]	                = $objeto->sys_module . "html/report_estatus_body";
+		$option["template_title"]	                = $objeto->sys_module . "html/report_title";
+		$option["template_body"]	                = $objeto->sys_module . "html/report_body";
 		
 				
 		$data										= $objeto->__REPORTE($option);		
 		$objeto->words["module_body"]				=$data["html"];
+		
 		$module_title								="Reporte de ";
     }
-    
+	$module_right=array(
+		array("create"=>"Crear"),			
+		array("report"=>"Reporte"),		    
+		array("report_sol"=>"","icon"=>"ui-icon-caret-1-s", "title"=>"Solicitud de Incapacidades"),	
+		array("report_dev"=>"","icon"=>"ui-icon-caret-1-n", "title"=>"Devolucion de Incapacidades"),	
+		array("report_fs"=>"FS", "title"=>"Solicitud de F1 y F3 para calificacion"),		    		    
+		array("report_no"=>"","icon"=>"ui-icon-closethick" , "title"=>"Calificada NO"),
+		array("report_si"=>"","icon"=>"ui-icon-check" , "title"=>"Calificada SI"),
+		array("report_carta_realizada"=>"","icon"=>"ui-icon-mail-closed" , "title"=>"Cartas Realizadas"),
+	);
 
-    if($objeto->__NIVEL_SESION("<=20")==true)	 // NIVEL ADMINISTRADOR 
-    {
-    	$module_right_admin=array(
-		    array("report_pendiente"=>"","icon"=>"ui-icon-help"),
-		    array("report_cancelados"=>"","icon"=>"ui-icon-closethick"),
-		    array("report_aprovados"=>"","icon"=>"ui-icon-check"),
-		);	    
-		$module_right=array_merge($module_right, $module_right_admin);		
-	/*	
-	}
-    if($objeto->__NIVEL_SESION("<=10")==true)   // NIVEL SUPER ADMINISTRADOR
-    {
-		*/
-    	$module_right_admin=array(
-		    array("report_especifico"=>"R Esp."),
-		    array("report_general"=>"R Gral."),
-		);	    
-		$module_right=array_merge($module_right, $module_right_admin);		
-	}
-
+	$boton_calificacion_si=array(		    
+		array("action_si_trabajo"=>"SI DE TRABAJO"),	
+		array("action_si_trayecto"=>"SI DE TRAYECTO"),	
+		array("action_si_recaida"=>"SI DE RECAIDA"),	
+	);
+	$boton_calificacion_no=array(		    
+		array("action_no_trabajo"=>"NO DE TRABAJO"),	
+		array("action_no_trayecto"=>"NO DE TRAYECTO"),			
+		array("action_no_recaida"=>"NO DE RECAIDA"),			
+	);
+	
     
-	$objeto->words["module_title"]              ="$module_title Calculo";
+	$objeto->words["module_title"]              ="$module_title Incapacidades";
+	$objeto->words["fecha_impresion"]           =$objeto->sys_date;
 	
+	$_SESSION["pdf"]["sys_titulo"]				="DIRECCION DE PRESTACIONES ECONOMICAS Y SOCIALES";
+	$_SESSION["pdf"]["sys_subtitulo"]			="COORDINACION DE SALUD EN EL TRABAJO";
 	
-	
-	$objeto->words["biometrico"]             =$objeto->__BUTTON(array(array("ver_biometrico"=>"Ver Biometrico")));
+	$objeto->words["asunto_edificio"]			="UNIDAD DE MEDICINA FAMILIAR No 17";
+	$objeto->words["pie_titular"]				="<b>DRA CINTHIA ZULEMA DIAZ</b>";
+	$objeto->words["pie_puesto"]				="MEDICO DE SALUD EN EL TRABAJO";
+	$objeto->words["pie_edificio"]				=$objeto->words["asunto_edificio"];
 	
 	
 	$objeto->words["module_left"]               =$objeto->__BUTTON($module_left);
 	$objeto->words["module_center"]             =$objeto->__BUTTON($module_center);
 	$objeto->words["module_right"]              =$objeto->__BUTTON($module_right);
+	$objeto->words["botones_calificacion_si"]   =$objeto->__BUTTON($boton_calificacion_si);
+	$objeto->words["botones_calificacion_no"]   =$objeto->__BUTTON($boton_calificacion_no);
 	
 	$objeto->words["html_head_description"]	=	"EN LA EMPRESA SOLESGPS, CONTAMOS CON UN MODULO PARA ADMINISTRAR EL REGISTRO DE USUARIOS DE LA PLATAFORMA DE RASTREO.";
 	$objeto->words["html_head_keywords"] 	=	"GPS, RASTREO, MANZANILLO, SATELITAL, CELULAR, VEHICULAR, VEHICULO, TRACTO, LOCALIZACION, COLIMA, SOLES, SATELITE, GEOCERCAS, STREET VIEW, MAPA";
 
 	$objeto->words["html_head_title"]           ="IMSS	 :: {$objeto->words["module_title"]}";
     
+	
     $objeto->html                               =$objeto->__VIEW_TEMPLATE("system", $objeto->words);
     $objeto->__VIEW($objeto->html);    
 ?>
