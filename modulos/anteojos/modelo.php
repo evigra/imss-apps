c401c558668a9c4dfa5e2e01ee89e3e9524e1c0f
Eduardo Vizcaino Granados <evigra@hotmail.com>
2019-05-30 00:04:03 -0500

    Update

--- modulos/anteojos/modelo.php
+++ modulos/anteojos/modelo.php
@@ -253,7 +253,7 @@
 					$words["fecha"]							=$this->sys_date;		
 					#$words["fecha"]							="2017-09-21";		
 																											
-					$template								.=$this->__TEMPLATE2("sitio_web/html/PDF_FORMATO_IMSS2_TIT2");
+					$template								.=$this->__TEMPLATE2("sitio_web/html/PDF_FORMATO_IMSS");
 					$template								=$this->__REPLACE($template,$words);
 				}	
 
